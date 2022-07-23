0a1b61dbebd4059f6f0a52ae49940906dbd29e72
Jordan D <degco44@gmail.com>
2022-07-23 11:31:58 -0500

    add SQL queries

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'gFLCjcNR4J5R',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not found)
 app.use((req, res) => {
